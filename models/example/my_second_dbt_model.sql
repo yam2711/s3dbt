0eae4d95a1242999bc27eea6d03ae8ffdf00a844
yams27112000@gmail.com <yams27112000@gmail.com>
2022-11-03 04:54:21 +0000

    five

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -7,11 +7,20 @@ select *
 from {{ref('my_first_dbt_model')}}
 ),
 
+b as (
+    select * from Analytics.public.ldsdata4
+),
+
 final as(
-    select  top 100 * from a 
-    where endstationname IN ('Vesey Pl & River Terrace', 'Madison St & Clinton St', 'Franklin St & W Broadway')
+    
+    select a.endstationname, b.endstationlongitude,  a.birthyear,
+    a.gender, a.usertype, a.bikeid
+    from a 
+    inner join b                                                                                                                                                                                               
+    on a.bikeid = b.bikeid
     
     )
 
 
-select * from final
+select top 100 * from final
+ where endstationname IN ('Vesey Pl & River Terrace', 'Madison St & Clinton St', 'Franklin St & W Broadway')
